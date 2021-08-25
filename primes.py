e4495e5baeb4956e676c02c9e556d21c824ace18
Renegade <carey3@gmail.com>
2021-08-25 14:28:20 -0500

    Create primes.py

---
+++ primes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+
+def isprime(n):
+    if n <= 1:
+        return False
+    for x in range(2, n):
+        if n % x == 0:
+            return False
+    else:
+        return True
+
+for n in range(100):
+    if isprime(n):
+        print(f'{n}', end= ' ', flush=True )
+ 
+
