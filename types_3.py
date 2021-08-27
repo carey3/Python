5c178f375d05306d6749edc903a1dcc658ef4175
Renegade <carey3@gmail.com>
2021-08-27 00:11:39 -0500

    updated types with multiple types

--- types_3.py
+++ types_3.py
@@ -2,5 +2,29 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 x = 7
-print('x is {}'.format(x))
-print(type(x))
+print(f'x is {format(x)} and type {type(x)}')
+
+x = 7.1
+print(f'x is {format(x)} and type {type(x)}')
+
+x = True
+print(f'x is {format(x)} and type {type(x)}')
+
+x = False
+print(f'x is {format(x)} and type {type(x)}')
+
+x = 7 > 5
+print(f'x is {format(x)} and type {type(x)}')
+
+x = None
+print(f'x is {format(x)} and type {type(x)}')
+
+x = "None"
+print(f'x is {format(x)} and type {type(x)}')
+
+x = 0
+print(f'x is {format(x)} and type {type(x)}')
+
+x = ""
+print(f'x is {format(x)} and type {type(x)}')
+
