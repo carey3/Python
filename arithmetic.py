3838059ae59b22a6345db8e28782e21af40662c5
Renegade <carey3@gmail.com>
2021-08-30 20:27:48 -0500

    arithmetic demo

---
+++ arithmetic.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+
+x = 15
+y = 3
+print(f'x is {x}\ny is {y}')
+
+z = x + y
+print(f' {x} + {y} is {z}')
+
+z = x - y
+print(f' {x} - {y} is {z}')
+
+z = x * y
+print(f' {x} * {y} is {z}')
+
+z = x / y
+print(f' {x} / {y} is {z}')
+
+z = x // y
+print(f' {x} // {y} is {z}')
+
+z = x % y
+print(f' {x} % {y} is {z}')
+
+z = x ** y
+print(f' {x} ** {y} is {z}')
