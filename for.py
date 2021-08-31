69bcea17bbc7d513db435b14c1033af412f2a1d2
Renegade <carey3@gmail.com>
2021-08-30 23:13:14 -0500

    from class examples

---
+++ for.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+
+animals = ( 'bear', 'bunny', 'dog', 'cat', 'velociraptor' )
+
+for pet in animals:
+    if pet == 'cat': continue
+    print(pet)
+
