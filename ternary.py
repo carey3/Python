85786e99cb614590cf320d2382c523c6abcb83e2
Renegade <carey3@gmail.com>
2021-08-30 21:11:33 -0500

    updates

--- ternary.py
+++ ternary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-hungry = True
+hungry = True # this must have the if and the else, you can have else None
 x = 'Feed the bear now!' if hungry else 'Do not feed the bear.'
 print(x)
